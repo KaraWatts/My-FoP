ddd926ea63f1c360ef2cabdca5bf781b33bc55b4
Kara Watts <kdwwatts@gmail.com>
2024-01-07 20:17:42 -0500

    improved local storage function

--- lesson_05/botScore/app.js
+++ lesson_05/botScore/app.js
@@ -3,13 +3,13 @@ document.querySelector("#plusone").addEventListener("click", addOne);
 document.querySelector("#minusone").addEventListener("click", minusOne);
 document.querySelector("#reset").addEventListener("click", reset);
 
-let total = 0
+let total = parseInt(localStorage["count"])
 
 function addOne(){
     total++
     document.querySelector('h4').innerText = total
     localStorage.setItem('count', total)
-console.log(total)
+console.log(localStorage)
 }
 function minusOne(){
     total--
