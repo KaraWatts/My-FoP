dc2f52a4a31da6d227271566f0a6d21aa97e205f
Kara Watts <kdwwatts@gmail.com>
2024-01-09 21:17:52 -0500

    improved numberguess

--- lesson_07/numberGuess/numberguess.py
+++ lesson_07/numberGuess/numberguess.py
@@ -1,17 +1,26 @@
-answer = int(input('Enter the integer for the player to guess: '))
+import random
+
+
+answer = random.randrange(start=100)
 
 count = 1
 while True:
-    guess = int(input('Guess a number: '))
-    if guess > answer:
-        print('Too high - try again')
-        count += 1
-        continue
-    if guess < answer:
-        print('Too low - try again')
-        count += 1
-        continue
-    if guess == answer:
-        print(f'You guessed it in {count} tries')
-        break
+    guess = input('Guess a number: ')
+    if guess.isnumeric():
+        guess = int(guess)
+        if guess > answer:
+            print('Too high - try again')
+            count += 1
+            continue
+        if guess < answer:
+            print('Too low - try again')
+            count += 1
+            continue
+        if guess == answer:
+            print(f'You guessed it in {count} tries')
+            break
+    else:
+        print('Not a valid guess only numbers please')
+
+
     
