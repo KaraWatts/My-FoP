ffefde784745d3dbcbc5de3f121794ada545908c
Kara Watts <kdwwatts@gmail.com>
2024-01-16 22:16:29 -0500

    optomized satData

--- lesson_10/satData/satdata.py
+++ lesson_10/satData/satdata.py
@@ -5,25 +5,32 @@ class SatData:
     def __init__(self, data_file):
 
         with open('sat.json', 'r') as infile:
-            self._data = json.load(infile)
-
-    def save_as_csv(self, dbn_list):
-        header_list = self._data['meta']['view']['columns']
+            self._data_all = json.load(infile)
+        header_list = self._data_all['meta']['view']['columns']
         headers =''
         for index in range(8, 13):
             headers = headers + header_list[index]['name'] + ','
+        formatted_data = {}
+        for school in self._data_all['data']:
+            formatted_row = ''  
+            for index in range(8, 13):
+                if school[index]:
+                    formatted_row = formatted_row + school[index] + ','
+                else:
+                    formatted_row += ','
+                formatted_data[school[8]] = formatted_row
+        self._data = formatted_data
+        self._header = headers
+
+
+    def save_as_csv(self, dbn_list):
         with open('output.csv', 'w') as outfile:
-            outfile.write(headers + ('\n'))
-            for school in self._data['data']:
-                for dbn in dbn_list:
-                    if dbn == school[8]:
-                            for index in range(8, 13):
-                                if school[index]:
-                                    outfile.write(school[index] + ',')
-                                    outfile.write(',')
-                                else:
-                            outfile.write('\n')
-        
+            outfile.write(self._header + ('\n'))
+            for dbn in dbn_list:
+                if dbn in self._data:
+                    outfile.write(self._data[dbn] + "\n") 
+                    
+            
         
 
 
