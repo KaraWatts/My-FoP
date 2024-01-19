b794cd834b0625078590ed10777f5e9839ee74de
Kara Watts <kdwwatts@gmail.com>
2024-01-18 21:04:27 -0500

    solved binarySearch

---
+++ lesson_12/binarySearch/binarysearch.py
@@ -0,0 +1,17 @@
+def binarySearch(list, value):
+    left = 0
+    right = len(list)-1
+    while left <= right:
+        middle = (left + right) // 2
+        if list[middle] == value:
+            return middle
+        elif list[middle] < value:
+            left = middle + 1
+        elif list[middle] > value:
+            right = middle -1
+    return -1
+
+# Test Cases
+print(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10)) # 2
+print(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95)) # 16
+print(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 100)) # -1
