5f418f659fb977a077e9e6a8664038c91d41aee8
Kara Watts <kdwwatts@gmail.com>
2024-01-19 21:45:12 -0500

    solved recursionIntro

--- lesson_12/recursionIntro/recursionintro.py
+++ lesson_12/recursionIntro/recursionintro.py
@@ -1,12 +1,12 @@
 def countdown(int):
     if int == 1:
         return str(int)
-    return (str(int) + ', '+countdown(int-1))
+    return (str(int) + ' '+countdown(int-1))
 
 def countup(int):
     if int == 1:
         return str(int)
-    return (countup(int-1) + ', ' + str(int))
+    return (countup(int-1) + ' ' + str(int))
 
 def sum_countup(int):
     if int == 1:
