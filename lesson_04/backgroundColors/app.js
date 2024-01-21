600d766908ccdb8a65e2aa84489b85eb2891c99a
Kara Watts <kdwwatts@gmail.com>
2024-01-20 21:51:10 -0500

    solved backgroundColors

--- lesson_04/backgroundColors/app.js
+++ lesson_04/backgroundColors/app.js
@@ -1,15 +1,35 @@
-document.querySelector("#turnRed").addEventListener("click", turnRedFunction);
 document.querySelector("#turnBlue").addEventListener("click", turnBlueFunction);
-document.querySelector("#turnPurple").addEventListener("click", turnPurpleFunction);
+document.querySelector("#turnGreen").addEventListener("click", turnGreenFunction);
+document.querySelector("#turnRed").addEventListener("click", turnRedFunction);
+document.querySelector("#turnYellow").addEventListener("click", turnYellowFunction);
+document.querySelector("#turnOrange").addEventListener("click", turnOrangeFunction);
+document.querySelector("#turnRandom").addEventListener("click", turnRandomFunction);
+
+
+
+
+function turnBlueFunction() {
+  document.querySelector("body").style.backgroundColor = "lightskyblue";
+}
 
 function turnRedFunction() {
   document.querySelector("body").style.backgroundColor = "red";
 }
 
-function turnBlueFunction() {
-  document.querySelector("body").style.backgroundColor = "blue";
+function turnGreenFunction() {
+  document.querySelector("body").style.backgroundColor = "green";
 }
 
-function turnPurpleFunction() {
-  document.querySelector("body").style.backgroundColor = "purple";
+function turnYellowFunction() {
+  document.querySelector('body').style.backgroundColor = 'gold';
 }
+
+function turnOrangeFunction() {
+  document.querySelector('body').style.backgroundColor = 'orange';
+}
+
+function turnRandomFunction() {
+  colors=['lightskyblue','green','gold','red','orange']
+  document.querySelector('body').style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+}
+
