4676a1e2851b447142a8b475adf876ed09a520dc
Kara Watts <kdwwatts@gmail.com>
2024-01-21 19:19:08 -0500

    solved dayofWeek

--- lesson_04/dayOfWeek/app.js
+++ lesson_04/dayOfWeek/app.js
@@ -4,6 +4,22 @@ function checkDay() {
   
   const day = document.querySelector('#day').value
 
-  // logic goes here
+  daysOfTheWeek = {
+    'saturday': 'weekend',
+    'sunday': 'weekend',
+    'monday': 'weekday',
+    'tuesday': 'weekday',
+    'wednesday': 'weekday',
+    'thursday': 'weekday',
+    'friday': 'weekday'
+  }
+  lowercaseDay = day.toLowerCase()
+  if (lowercaseDay in daysOfTheWeek){
+    document.querySelector('#placeToSee').innerText = daysOfTheWeek[lowercaseDay]
+  } else {
+    document.querySelector('#placeToSee').innerText = 'Not a valid day of the week. Please try again.'
+  }
 
+
+  
 }
