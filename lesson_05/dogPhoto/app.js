9a42a8bb28d54e755c43c39b016c6cf35fe1e2c5
Kara Watts <kdwwatts@gmail.com>
2024-01-21 19:55:26 -0500

    solved dogPhoto

--- lesson_05/dogPhoto/app.js
+++ lesson_05/dogPhoto/app.js
@@ -2,6 +2,7 @@ fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
+      document.querySelector('img').src = data['message']
     })
     .catch(err => {
         console.log(`error ${err}`)
