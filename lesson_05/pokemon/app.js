39f4adb2459d26e916fb5892558be7eadaf12e36
Kara Watts <kdwwatts@gmail.com>
2024-01-21 21:09:10 -0500

    solved pokemon

--- lesson_05/pokemon/app.js
+++ lesson_05/pokemon/app.js
@@ -8,4 +8,21 @@ async function getFetch() {
     let pokemonInfo = await data.json()
 
     console.log(pokemonInfo)
+
+    let shiny = pokemonInfo.sprites.front_shiny
+    let defaultPic = pokemonInfo.sprites.front_default
+    let type = pokemonInfo.types[0]['type']['name']
+    let name = pokemonInfo.name
+    
+    document.querySelector('#pokemonName').innerText = 'Name: ' + name
+    document.querySelector('#type').innerText = 'Type: ' + type
+
+    if (document.querySelector('#shinyBox').checked){
+        document.querySelector('img').src = shiny
+    }else{
+    document.querySelector('img').src = defaultPic
+    }
+
+
 }
+
